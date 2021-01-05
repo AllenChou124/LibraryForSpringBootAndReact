995efa167b1aaf789bb18f52b494d186af6dc54a
AllenChou124 <zhoutmicallen@gmail.com>
2021-01-05 21:59:25 +0800

    feat init-01

---
+++ lib-web/lib-web-api/src/main/java/com/zhou/lib/api/UserService.java
@@ -0,0 +1,14 @@
+package com.zhou.lib.api;
+
+
+import com.zhou.lib.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
